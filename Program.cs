68db591bcd6673d05d889452207a4536fd36eb7c
Andrés Camilo Piratova Pineda <azprodiyi@gmail.com>
2023-05-26 22:03:50 -0500

    Bugs
    Stop the project for knowledge

--- Program.cs
+++ Program.cs
@@ -1,12 +1,22 @@
 using GraphiQl;
 using GraphQL.Server;
 using GraphQL.Types;
+using GraphQLProject.Data;
 using GraphQLProject.Interfaces;
 using GraphQLProject.Mutation;
 using GraphQLProject.Query;
 using GraphQLProject.Schema;
 using GraphQLProject.Services;
 using GraphQLProject.Type;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,10 +24,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddTransient<IProduct, ProductService>();
-builder.Services.AddSingleton<ProductType>();
-builder.Services.AddSingleton<ProductQuery>();
-builder.Services.AddSingleton<ProductMutation>();
-builder.Services.AddSingleton<ISchema, ProductSchema>();
+builder.Services.AddTransient<ProductType>();
+builder.Services.AddTransient<ProductQuery>();
+builder.Services.AddTransient<ProductMutation>();
+builder.Services.AddTransient<ISchema, ProductSchema>();
 
 builder.Services.AddGraphQL(options =>
 {
@@ -26,6 +36,9 @@ builder.Services.AddGraphQL(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<GraphQLDbContext>(options => options.UseSqlServer(@"Data Source= (localdb)\MSSQLLocalDB;Initial Catalog=GraphQLDb;Integrated Security = True"));
+builder.Services.AddControllers();
+
 
 var app = builder.Build();
 
--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -1,40 +1,48 @@
-﻿using GraphQLProject.Interfaces;
+﻿using GraphQLProject.Data;
+using GraphQLProject.Interfaces;
 using GraphQLProject.Models;
 
 namespace GraphQLProject.Services
 {
     public class ProductService : IProduct 
     {
-        private static List<Product> products = new List<Product>()
+        private GraphQLDbContext _dbContext;
+
+        public ProductService(GraphQLDbContext dbContext)
         {
-            new Product(){Id = 0, Name = "Coffee", Price = 10},
-            new Product(){Id = 1, Name = "Tea", Price = 15},
-            new Product(){Id = 2, Name = "Chocolate", Price = 20},
-        };
+            _dbContext = dbContext;
+        }
+
         public Product AddProduct(Product product)
         {
-            products.Add(product);
+            _dbContext.Products.Add(product);
+            _dbContext.SaveChanges();
             return product;
         }
 
         public void DeleteProduct(int id)
         {
-            products.RemoveAt(id);
+            var productObj = _dbContext.Products.Find(id);
+            _dbContext.Products.Remove(productObj);
+            _dbContext.SaveChanges();
         }
 
         public List<Product> GetAllProducts()
         {
-            return products;
+            return _dbContext.Products.ToList();
         }
 
         public Product GetProductById(int id)
         {
-            return products.Find(p  => p.Id == id);
+            return _dbContext.Products.Find(id);
         }
 
         public Product UpdateProduct(int id, Product product)
         {
-            products[id] = product;
+            var productObj = _dbContext.Products.Find(id);
+            productObj.Name = product.Name;
+            productObj.Price = product.Price;
+            _dbContext.SaveChanges();
             return product;
         }
     }
